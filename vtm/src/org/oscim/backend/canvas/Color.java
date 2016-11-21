f84608f162d2d9f30be8f4941acc27c49cfe7ee8
Izumi Kawashima <schedul.xor@gmail.com>
2016-11-22 01:29:21 +0900

    Separated RRGGBB parser from colorString parser

--- vtm/src/org/oscim/backend/canvas/Color.java
+++ vtm/src/org/oscim/backend/canvas/Color.java
@@ -210,20 +210,24 @@ public final class Color {
      */
     public static int parseColor(String colorString) {
         if (colorString.charAt(0) == '#') {
-            // Use a long to avoid rollovers on #ffXXXXXX
-            long color = Long.parseLong(colorString.substring(1), 16);
-            if (colorString.length() == 7) {
-                // Set the alpha value
-                color |= 0x00000000ff000000;
-            } else if (colorString.length() != 9) {
-                parseColorException(colorString);
-            }
-            return (int) color;
+            return parseArgbColor(colorString.substring(1));
         }
         if (colorString.charAt(0) == 'r') {
             return parseColorComponents(colorString);
         }
         throw new IllegalArgumentException("Unknown color");
+    }
+
+    public static int parseArgbColor(String pureColorString) {
+        // Use a long to avoid rollovers on #ffXXXXXX
+        long color = Long.parseLong(pureColorString, 16);
+        if (pureColorString.length() == 6) {
+            // Set the alpha value
+            color |= 0x00000000ff000000;
+        } else if (pureColorString.length() != 8) {
+            parseColorException(pureColorString);
+        }
+        return (int) color;
     }
 
     public static int parseColor(String colorString, int fallBackColor) {
