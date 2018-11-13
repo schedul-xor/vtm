53e56eef77f0556e696003b4ac43027e89d5f398
Izumi Kawashima <schedul.xor@gmail.com>
2018-11-13 21:38:46 +0900

    Changed namespace

--- vtm-gdx-poi3d/src/org/oscim/gdx/poi3d/ModelHolder.java
+++ vtm-gdx-poi3d/src/org/oscim/gdx/poi3d/ModelHolder.java
@@ -12,7 +12,7 @@
  * You should have received a copy of the GNU Lesser General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package org.oscim.model.poi3d;
+package org.oscim.gdx.poi3d;
 
 import com.badlogic.gdx.graphics.g3d.Model;
 
